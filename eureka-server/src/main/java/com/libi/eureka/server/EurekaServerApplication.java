24cbe22aefc29e2500facd3b7a3bee85e1ca3b77
李比 <843298330@qq.com>
2019-08-27 10:21:33 +0800

    feat(注册中心server): 添加了EurekaServer的相关写法

---
+++ eureka-server/src/main/java/com/libi/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.libi.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author :Libi
+ * @version :1.0
+ * @date :2019-08-27 10:03
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
